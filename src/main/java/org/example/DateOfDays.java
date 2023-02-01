cad9718c9db362b902dbe9eb02e35d2b363cc59c
Aditya Jain <get.aditya01@gmail.com>
2023-02-01 13:12:19 +0100

    New Commit for 100days fo date

---
+++ src/main/java/org/example/DateOfDays.java
@@ -0,0 +1,17 @@
+    package org.example;
+
+    import java.time.LocalDate;
+    import java.time.Period;
+    import java.util.Date;
+
+    public class DateOfDays {
+
+        private static LocalDate calculateHundredDaysFromNow(LocalDate today) {
+        return today.plusDays(100);
+        }
+
+        public static void main(String[] args) {
+            LocalDate today =  LocalDate.now();
+            System.out.println(calculateHundredDaysFromNow(today));
+        }
+    }
